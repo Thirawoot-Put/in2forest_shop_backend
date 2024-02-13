dec66289c8b73745cf3010b06d98e4c916890d06
Thirawoot-put <thirawoot.put@gmail.com>
2024-02-13 09:49:10 +0700

    add neccessary middlewares

--- src/app.js
+++ src/app.js
@@ -3,9 +3,22 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-
+const notFound = require('./middlewares/not-fount');
+const error = require('./middlewares/error');
+const limiter = require('./middlewares/rate-limit');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(limiter);
+app.use(morgan('dev'));
+// app.use('/public', express.static('public'));
+
+app.use('/', (req, res, next) => { res.json({ message: 'hello' }) })
+
+app.use(notFound);
+app.use(error);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
---
+++ src/middlewares/error.js
@@ -0,0 +1,5 @@
+module.exports = (err, req, res, next) => {
+    console.log(err);
+
+    res.status(err.statusCode || 500).json({ message: err.message });
+};
