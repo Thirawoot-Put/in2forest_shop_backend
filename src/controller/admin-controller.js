d85ffc1d012c76911dd2a5f1681bc55b607b5d5b
Thirawoot-put <thirawoot.put@gmail.com>
2024-02-16 23:10:54 +0700

    edit-product

--- src/controller/admin-controller.js
+++ src/controller/admin-controller.js
@@ -22,15 +22,30 @@ exports.addProduct = async (req, res, next) => {
     }
 };
 
-exports.editProduct = catchError(async (req, res, next) => {
-    const productId = +req.params.productId
-    const product = await adminService.findProductById(productId);
-    if (!product) {
-        res.status(400).json({ message: "Product not found" })
+exports.editProduct = async (req, res, next) => {
+    try {
+        const productId = +req.params.productId
+        const product = await adminService.findProductById(productId);
+        if (!product) {
+            res.status(400).json({ message: "Product not found" })
+        }
+        const { price, productTypeId } = req.body
+        const changeTypeData = { ...req.body, price: +price, productTypeId: +productTypeId }
+        const editProduct = await adminService.editProductById(productId, changeTypeData)
+        if (req.file) {
+            const data = {};
+            data.mainImage = await uploadService.upload(req.file.path)
+            updateController(adminService.uploadMainImage, data, editProduct.id)
+        }
+        res.status(200).json({ editProduct });
+    } catch (error) {
+        console.log(error)
+    } finally {
+        if (req.file) {
+            fs.unlink(req.file.path);
+        }
     }
-    const editProduct = await adminService.editProductById(productId, req.body)
-    res.status(200).json({ editProduct });
-});
+};
 
 exports.removeProduct = catchError(async (req, res, next) => {
     const productId = +req.params.productId;
@@ -50,4 +65,10 @@ exports.getAllProductTypes = catchError(async (req, res, next) => {
 exports.getAllProduct = catchError(async (req, res, next) => {
     const allProduct = await adminService.getAllProduct()
     res.status(200).json({ allProduct })
+})
+
+exports.getProductById = catchError(async (req, res, next) => {
+    const productId = +req.params.productId
+    const oldData = await adminService.findProductById(productId);
+    res.status(200).json({ oldData })
 })
--- src/routes/admin-route.js
+++ src/routes/admin-route.js
@@ -6,9 +6,10 @@ const upload = require('../middlewares/upload');
 const router = express.Router();
 
 router.post('/product', upload.single('mainImage'), adminController.addProduct);
-router.patch('/product/:productId', adminController.editProduct);
+router.patch('/product/:productId', upload.single('mainImage'), adminController.editProduct);
 router.delete('/product/:productId', adminController.removeProduct);
 router.get('/product/types', adminController.getAllProductTypes);
 router.get('/product', adminController.getAllProduct);
+router.get('/product/:productId', adminController.getProductById)
 
 module.exports = router;
